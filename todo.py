6c1d8af1cbd438b929424ac8d99de50d8f58a36e
mhancherick <141765650+mhancherick@users.noreply.github.com>
2025-06-17 16:22:21 -0400

    Create add_task function

--- todo.py
+++ todo.py
@@ -4,10 +4,15 @@ class ToDoList:
     def __init__(self):
         self.tasks = []
 
+    def add_task(self, name, date, description=None, priority, completed):
+        self.tasks.append(Task(name, date, description, priority, completed))
+
+
 
 class Task:
-    def __init__(self, task_name, task_date=datetime.date.today(), task_description=None, task_priority=5):
-        self.task_name = task_name
-        self.task_date = task_date
-        self.task_description = task_description
-        self.task_priority = task_priority
+    def __init__(self, name, date=datetime.date.today(), description=None, priority=5, completed=False):
+        self.name = name
+        self.date = date
+        self.description = description
+        self.priority = priority
+        self.completed = completed
